b26c80a661fdd1cefab709b3c387ed5ae37e54d8
lixingfu <lifengyuzhe@163.com>
2016-09-01 23:45:41 +0800

    2016/9/1

--- 笔记和学习记录/其他/HeadSort.java
+++ 笔记和学习记录/其他/HeadSort.java
@@ -20,11 +20,29 @@ public class HeadSort {
 	 * 
 	 * @param a
 	 */
-	public void swim(int[] a, int n) {
-		int N = a.length;
-		while (a[n] > a[n / 2] && n <= N && n / 2 > 1) {
-			Utils.swap(a, n, n / 2);
+	public static void swim(int[] arr, int index) {
+		int N = arr.length-1;
+
+		while (index > 0 && index <= N) {
+			int left = index*2;
+			int right = index*2 +1;
+			
+			//有左子树而没有右子树
+			if(left<=N&&right>N){
+				if(arr[left]>arr[index]	)
+					Utils.swap(arr, left, index);
+			}
+			//有左子树也有右子树
+			if(left<=N && right<=N){
+				int max ;
+				max=(arr[left]-arr[right]<0?right:left);
+				if(arr[max]>arr[index])
+					Utils.swap(arr, index, max);
+			}
+			
+			index=index/2;
 		}
+
 	}
 
 	/**
@@ -33,7 +51,7 @@ public class HeadSort {
 	 * @param a
 	 * @param n
 	 */
-	public void sink(int[] a, int n) {
+	public static void sink(int[] a, int n) {
 		int N = a.length;
 		while ((a[n] < a[2 * n] || a[n] < a[2 * n + 1]) && n <= N) {
 			if (a[2 * n] > a[2 * n + 1])
@@ -45,18 +63,37 @@ public class HeadSort {
 
 	/**
 	 * 构建最大堆
+	 * 
 	 * @param a
 	 */
-	public void bulidMaxHeap(int[] a ){
-		
-		
+	public static void bulidMaxHeap(int[] arr) {
+		int length = arr.length;
+		// 先从最后一个节点元素的父节点开始
+		for(int i=length/2;i>0;i--){
+			swim(arr,i);
+		}
+
 	}
-	
-	
+
+	public static void mainHeapSort(int[] arr) {
+		int length = arr.length;
+		bulidMaxHeap(arr);
+
+	}
+
 	public static void main(String[] args) {
 
-		int[] arr = { 3, 5, 8, 4, 2, 7, 6, 1, 9, 13, 12, 11 };
-
+		int[] arrtemp = { 3, 5, 8, 4, 2, 7, 6, 1, 9, 13, 12, 11 };//12个
+		int[] arr =new int[arrtemp.length+1];
+		for(int i=0;i<arrtemp.length;i++){
+			arr[i+1]=arrtemp[i];
+					
+		}
+		System.out.println("排序前:");
+		Utils.printNums(arr);
+		mainHeapSort(arr);
+		System.out.println("排序后:");
+		Utils.printNums(arr);
 	}
 
 }
