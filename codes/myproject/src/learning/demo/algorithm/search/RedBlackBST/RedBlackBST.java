5a94200541c2367c0fff3f69fd1191f170cb67b8
lixingfu <lifengyuzhe@163.com>
2016-10-09 23:06:56 +0800

    2

--- codes/myproject/src/learning/demo/algorithm/search/RedBlackBST/RedBlackBST.java
+++ codes/myproject/src/learning/demo/algorithm/search/RedBlackBST/RedBlackBST.java
@@ -4,6 +4,7 @@ package learning.demo.algorithm.search.RedBlackBST;
 public class RedBlackBST<Key extends Comparable<Key>, Value> {
 	private static final boolean RED = true;
 	private static final boolean BLACK = false;
+	private Node root;
 
 	private class Node {
 		Key key;
@@ -21,9 +22,42 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 		}
 	}
 
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node x) {
+		if (x == null)
+			return 0;
+		else
+			return x.N;
+	}
+
 	private boolean isRed(Node x) {
 		if (x == null)
 			return false;
 		return x.color == RED;
 	}
+
+	Node rotateLeft(Node h) {
+		Node x = h.right;
+		x.left = h;
+		x.color = h.color;
+		h.color = RED;
+		x.N = h.N;
+		h.N = 1 + size(h.left) + size(h.right);
+		return x;
+	}
+
+	Node rotateRight(Node h) {
+		Node x = h.left;
+		x.right = h;
+		x.color = h.color;
+		h.color = RED;
+		x.N = h.N;
+		h.N = 1 + size(h.left) + size(h.right);
+		return x;
+
+	}
+
 }
