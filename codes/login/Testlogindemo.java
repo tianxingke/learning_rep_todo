23603339955adec339ee6c43c9896c4e02d28273
tianxingke <lifengyuzhe@163.com>
2016-10-10 18:23:51 +0800

    2

---
+++ codes/login/Testlogindemo.java
@@ -0,0 +1,11 @@
+package com.demo.login;
+
+public class Testlogindemo {
+	public static void main(String[] args) throws Exception {
+//		Sign sign = new Sign();
+//		sign.sign();
+		
+		SignOut so = new SignOut();
+		so.signout();
+	}
+}
